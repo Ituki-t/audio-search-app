4074a9b5fe6dc9c3898d51a9e745da0e58ce8d9e
Itsuki-T <sanndaittapandaotta@gmail.com>
2026-02-13 18:09:40 +0900

    fileのアップロード機能を実装, upload.htmlのformには、enctype=multipart/form-dataが必要

--- voices/urls.py
+++ voices/urls.py
@@ -5,6 +5,6 @@ from . import views
 app_name = 'voices'
 urlpatterns = [
     path('', views.index, name='index'),
-    # path('upload/', views.upload_voice, name='upload_voice'),
+    path('upload/', views.upload, name='upload'),
     # path('voice/<int:voice_id>/', views.voice_detail, name='voice_detail'),
 ]
--- voices/views.py
+++ voices/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.shortcuts import get_object_or_404
+from django.shortcuts import redirect
 from .models import Voice
+from .forms import UploadVoiceForm
 
 
 # Create your views here.
@@ -8,3 +10,14 @@ from .models import Voice
 def index(request):
     voice = Voice.objects.all()
     return render(request, 'voices/index.html', {'voice': voice})
+
+
+def upload(request):
+    if request.method == 'POST':
+        form = UploadVoiceForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('voices:index')
+    else:
+        form = UploadVoiceForm()
+    return render(request, 'voices/upload.html', {'form': form})
