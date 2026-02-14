80b0e0eed6bd8aefa5f88bb4e5c2d14739ed2b96
Itsuki-T <sanndaittapandaotta@gmail.com>
2026-02-14 18:55:55 +0900

    add transcribe_status field

--- voices/models.py
+++ voices/models.py
@@ -6,6 +6,9 @@ class Voice(models.Model):
     title = models.CharField(max_length=200)
     audio_file = models.FileField(upload_to='voices/')
     uploaded_at = models.DateTimeField(auto_now_add=True)
-
+    transcribe_status = models.CharField(
+        max_length=20,
+        default="pending" # pending/running/done/failed
+        )
     def __str__(self):
         return self.title
