4dd6f87ba784fe1e4b8df8de7513ae8274996ea7
Itsuki-T <sanndaittapandaotta@gmail.com>
2026-02-18 19:14:06 +0900

    remove text2es

--- voices/views.py
+++ voices/views.py
@@ -7,8 +7,6 @@ from django.http import FileResponse
 from .models import Voice
 from .forms import UploadVoiceForm
 from .tasks import transcribe_voice
-from .tasks import text2es
-
 
 # Create your views here.
 
@@ -26,7 +24,6 @@ def upload(request):
             voice.title = title
             voice.save()
             transcribe_voice.delay(voice.id)
-            # text2es("voice_index", voice.id, transcribe)
             return redirect('voices:index')
     else:
         form = UploadVoiceForm()
