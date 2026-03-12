78d9239958214d687ad1b1022bf6f66c4c0c1d52
Itsuki-T <sanndaittapandaotta@gmail.com>
2026-03-12 15:01:33 +0900

    admin segment

--- voices/admin.py
+++ voices/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from .models import Voice
+from .models import Segment
 
 # Register your models here.
 
@@ -7,4 +8,9 @@ from .models import Voice
 class VoiceAdmin(admin.ModelAdmin):
     list_display = ('id', 'title', 'uploaded_at')
 
+class SegmentAdmin(admin.ModelAdmin):
+    list_display = ('id', 'voice', 'start_time', 'end_time', 'text')
+
+
 admin.site.register(Voice, VoiceAdmin)
+admin.site.register(Segment, SegmentAdmin)
